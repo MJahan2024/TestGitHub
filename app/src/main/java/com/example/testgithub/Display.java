bbe42614cce9d6c42d7d9804e40e3c11af4edbc4
mjahan <mjahan@tru.ca>
2026-02-21 12:06:54 -0800

    updated Display

--- app/src/main/java/com/example/testgithub/Display.java
+++ app/src/main/java/com/example/testgithub/Display.java
@@ -3,6 +3,7 @@ package com.example.testgithub;
 public class Display {
     public String display(){
 //This method is returning my name
+        // updated inside Android
         return "Munima";
     }
 }
